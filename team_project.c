7c01ae5f1d9ce94b4cdd9e77e34bef7d62ee9954
yundoun <dbsehdns12@gmail.com>
2023-11-26 17:30:15 +0900

    Feat : 쉘 프로그램 초안 작성

---
+++ team_project.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main()
+{
+    char buf[256];
+    char *argv[50];
+    int narg;
+    pid_t pid;
+    while (1)
+    {
+        printf("shell> ");
+        gets(buf);
+        narg = getargs(buf, argv);
+        pid = fork();
+        if (pid == 0)
+            execvp(argv[0], argv);
+        else if (pid > 0)
+            wait((int *)0);
+        else
+            perror("fork failed");
+    }
+}
+int getargs(char *cmd, char **argv)
+{
+    int narg = 0;
+    while (*cmd)
+    {
+        if (*cmd == ' ' || *cmd == '\t')
+            *cmd++ = '\0';
+        else
+        {
+            argv[narg++] = cmd++;
+            while (*cmd != '\0' && *cmd != ' ' && *cmd != '\t')
+                cmd++;
+        }
+    }
+    argv[narg] = NULL;
+    return narg;
+}
